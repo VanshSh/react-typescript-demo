2a343f9056de585866790018413aa3b9be21ed41
VanshSh <vansh@nowandme.com>
2022-05-27 18:09:42 +0530

    Typescript assertion

--- src/state/User.tsx
+++ src/state/User.tsx
@@ -6,22 +6,22 @@ type AuthUser = {
 }
 
 export const User = () => {
-    const [user, setUser] = useState<AuthUser | null>(null)
+    const [user, setUser] = useState<AuthUser>({} as AuthUser)
     const handleLogin = () => {
         setUser({
             name: 'Vansh Sharma',
             email: 'vanshsharma9354@gmail.com',
         })
     }
-    const handleLogout = () => {
-        setUser(null)
-    }
+    // const handleLogout = () => {
+    //     setUser(null)
+    // }
     return (
         <div>
             <button onClick={handleLogin}>Login</button>
-            <button onClick={handleLogout}>Logout</button>
-            <h3>User name is {user?.name} </h3>
-            <h3>User name is {user?.email} </h3>
+            {/* <button onClick={handleLogout}>Logout</button> */}
+            <h3>User name is {user.name} </h3>
+            <h3>User name is {user.email} </h3>
         </div>
     )
 }
