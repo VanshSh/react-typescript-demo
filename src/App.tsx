b56fb620f7e11e75a06fffa27439505bec100429
VanshSh <vansh@nowandme.com>
2022-05-27 15:32:22 +0530

    Typo

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import './App.css'
 function App() {
     return (
         <div className='App'>
-            <h1>React Typescript code</h1>
+            <h1>React Typescript code.</h1>
         </div>
     )
 }
